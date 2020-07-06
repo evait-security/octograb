fc38c98a901b3db09343b950c56855b6dcce685f
Paul Werther <pw@evait.de>
2020-07-06 10:29:52 +0200

    add progressbar

--- octograb.rb
+++ octograb.rb
@@ -2,6 +2,7 @@ require "typhoeus"
 require "resolv"
 require 'colorize'
 require 'optparse'
+require 'ruby-progressbar'
 
 params = {}
 follow_r = false
@@ -73,6 +74,12 @@ end
 
 path = params[:path] || "/"
 dns = Resolv::DNS.open
+progressbar = ProgressBar.create(:title => "Progress",
+    :starting_at => 0,
+    :total => IO.readlines(inputfile).size * 4,
+    :format => "%a %b\u{15E7}%i %p%% %t",
+    :progress_mark  => ' ',
+    :remainder_mark => "\u{FF65}")
 
 File.readlines(inputfile).each do |line|
     # do not overload the que
@@ -80,11 +87,13 @@ File.readlines(inputfile).each do |line|
 
     r1 = Typhoeus::Request.new("http://#{line.strip}#{path}", followlocation: follow_r, timeout: 1)
     r1.on_complete do |response|
-        puts "[+] Content match: #{r1.url}".green if response.body.include? content
+        progressbar.increment
+        progressbar.log "[+] Content match: #{r1.url}".green if response.body.include? content
     end
     r2 = Typhoeus::Request.new("https://#{line.strip}#{path}", followlocation: follow_r, ssl_verifyhost: 0, timeout: 1)
     r2.on_complete do |response|
-        puts "[+] Content match: #{r2.url}".green if response.body.include? content
+        progressbar.increment
+        progressbar.log "[+] Content match: #{r2.url}".green if response.body.include? content
     end
     hydra.queue(r1)
     hydra.queue(r2)
@@ -95,17 +104,21 @@ File.readlines(inputfile).each do |line|
             ip = dns.getaddress(line.strip)
             r3 = Typhoeus::Request.new("http://#{ip}#{path}", followlocation: follow_r, timeout: 1)
             r3.on_complete do |response|
-                puts "[+] Content match: #{r3.url}".green if response.body.include? content
+                progressbar.increment
+                progressbar.log "[+] Content match: #{r3.url}".green if response.body.include? content
             end
             r4 = Typhoeus::Request.new("https://#{ip}#{path}", followlocation: follow_r, timeout: 1, ssl_verifyhost: 0)
             r4.on_complete do |response|
-                puts "[+] Content match: #{r4.url}".green if response.body.include? content
+                progressbar.increment
+                progressbar.log "[+] Content match: #{r4.url}".green if response.body.include? content
             end
             hydra.queue(r3)
             hydra.queue(r4)
         rescue
-            
+            progressbar.total -= 2
         end
+    else
+        progressbar.total -= 2
     end
 end
 
